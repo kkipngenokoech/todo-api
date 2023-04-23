64d03f8f3c0e20ef41f7aa374602ba4fd5149859
kip <kkipngenokoech22@gmail.com>
2023-04-23 09:27:45 +0300

    todo projecct and apps

--- _todos/asgi.py
+++ _todos/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for _todo project.
+ASGI config for _todos project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_todo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_todos.settings')
 
 application = get_asgi_application()
--- _todos/wsgi.py
+++ _todos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for _todo project.
+WSGI config for _todos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_todo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_todos.settings')
 
 application = get_wsgi_application()
