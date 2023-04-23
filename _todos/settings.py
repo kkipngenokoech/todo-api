ce3afd1d33977cc93b4457783dcad3b94acac6c5
kip <kkipngenokoech22@gmail.com>
2023-04-23 09:30:58 +0300

    created a suiper user

--- _todos/settings.py
+++ _todos/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local applications
+    'todos',
+    
+    # inbuilt applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
